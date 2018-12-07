94889608bd4cbf360bc43d541a644ab1b34e827f
Rijnard van Tonder <rvantonder@gmail.com>
2018-12-07 10:12:02 -0800

    Update crypto.py

--- baseplate/crypto.py
+++ baseplate/crypto.py
@@ -63,7 +63,7 @@ else:
         expected_len = len(expected)
         result = actual_len ^ expected_len
         if expected_len > 0:
-            for i in xrange(actual_len):
+            for i in range(actual_len):
                 result |= ord(actual[i]) ^ ord(expected[i % expected_len])
         return result == 0
 
